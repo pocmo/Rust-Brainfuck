88fc8a4d930e02a5421299b2fe92c59523d1d688
Sebastian Kaspari <s.kaspari@gmail.com>
2015-05-12 22:03:59 +0200

    Read program code from file (first argument).

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use std::collections::HashMap;
 use std::vec::Vec;
+use std::io::prelude::*;
+use std::fs::File;
+use std::env;
+use std::error::Error;
 
 fn build_loop_map(tokens: &Vec<char>) -> (HashMap<usize,usize>, HashMap<usize,usize>) {
     let mut forward_map = HashMap::new();
@@ -25,10 +29,32 @@ fn build_loop_map(tokens: &Vec<char>) -> (HashMap<usize,usize>, HashMap<usize,us
     return (forward_map, backward_map)
 }
 
-fn main() {
-    let program_code = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.".to_string();
+fn read_file(filename: &String) -> String {
+    let mut file = match File::open(filename) {
+        Err(why) => panic!("Could not open file {}: {}", filename, Error::description(&why)),
+        Ok(file) => file
+    };
 
-    let mut cells : [i8; 30000] = [0; 30000];
+    let mut program_code = String::new();
+    match file.read_to_string(&mut program_code) {
+        Err(why) => panic!("Could not read file {}: {}", filename, Error::description(&why)),
+        Ok(_) => {}
+    };
+
+    return program_code;
+}
+
+fn read_filename_from_arguments() -> String {
+    let args: Vec<_> = env::args().collect();
+    if args.len() != 2 {
+        panic!("Usage: cargo run yourfile.bf");
+    }
+
+    return args[1].clone();
+}
+
+fn evaluate(program_code: String) {
+        let mut cells : [i8; 30000] = [0; 30000];
     let tokens = program_code.chars().collect();
 
     let (forward_map, backward_map) = build_loop_map(&tokens);
@@ -62,4 +88,10 @@ fn main() {
         }
         code_pointer += 1;
     }
+}
+
+fn main() {
+    let filename = read_filename_from_arguments();
+    let program_code = read_file(&filename);
+    evaluate(program_code);
 }
